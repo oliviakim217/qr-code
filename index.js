b33a521280ac5399ec1a7d8b42b94dad6c828ea9
Olivia <oliviakim217@gmail.com>
2023-08-14 21:52:00 -0400

    Install the inquirer npm package to take user input.

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
+import inquirer from 'inquirer';
+
+const questions = [
+    {
+        type: 'input',
+        name: 'url',
+        message: 'Please enter a URL: ',
+        when(answers) {
+            return answers.comments !== 'Thanks!';
+        }
+    }
+];
+
+inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, '  '));
+});
